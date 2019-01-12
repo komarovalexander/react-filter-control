d2d59b0ba769f01a223c780a8d5190a4dfe167aa
Alexander <sanrkom@gmail.com>
2019-01-12 16:57:04 +0300

    Fix linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "parser": "babel-eslint",
     "rules":{
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "no-shadow": "off"
     },
     "env": {
         "jest": true,
